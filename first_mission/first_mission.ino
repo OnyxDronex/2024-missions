5a7b6d812dcca82433bd43164631754f89d90c73
Ben alaluf <94129183+benalaluf@users.noreply.github.com>
2024-03-10 15:51:45 +0200

    Update first_mission.ino

--- first_mission/first_mission.ino
+++ first_mission/first_mission.ino
@@ -1,6 +1,8 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
 
+#define SENSOR_ADDRESS 0x48
 
 #include <Servo.h>
 #include <Wire.h>
@@ -19,32 +21,51 @@ Adafruit_NeoPixel ring = Adafruit_NeoPixel(RING_LED_NUM, LED_RING_PIN, NEO_GRB +
 
 
 
-//delayshit
-
-
-
-
-
-
-
 
 int distance;
 unsigned long currentMillis;
 unsigned long previousMillis;
 
+int roomtemp;
+
 //------------------------------------------------------------
 void setup() {
   Serial.begin(9600);
+  Wire.begin();
   initSystem();
+
+  roomtemp = readTemperature();
 }
 
 
 
 void loop() {
   currentMillis = millis();
+  float temperature = readTemperature();
   int d = getCurrentDistance();
-  turnOnRings(200,0,200,d);
-  delay(200);
+  int num;
+
+  if (d > 50) {
+    num = 1;
+    num = (d - 50) / 25;
+  } else {
+    num = 0;
+  }
+
+  if (temperature > roomtemp + 2) {
+    blink(200, 0, 0, 300, num);
+
+  } else if (temperature < roomtemp - 0.5) {
+    blink(0, 0, 200, 300, num);
+
+  } else {
+    turnOnRings(0, 200, 0, num);
+  }
+  Serial.print("distance = ");
+  Serial.print(d);
+
+  Serial.print(" temp = ");
+  Serial.println(temperature);
 }
 //------------------------------------------------------------
 
@@ -68,56 +89,64 @@ void initRing() {
   ring.show();
 }
 
-//updates
 
-void turnOnRings(int red, int green, int blue, int distance) {
-  int num;
+void turnOnRings(int red, int green, int blue, int num) {
   ring.clear();
-  if(distance > 50){
-    num =1;
-    num *= (distance-50)/25;
-  }else{
-    num = -1;
+  if (num != 0) {
+    ring.fill(ring.Color(red, green, blue), 0, num);
   }
-    blink(red,green,blue,500,num);
-
+  ring.show();  // Update Neopixel Ring
 }
 
 
-
-
-//geters
 
 int getCurrentDistance() {
 
   word duration, distance;
   duration = pulseIn(ECHO_PIN, HIGH);
   distance = duration / 58;
-  Serial.print("distance = ");
-  Serial.println(distance);
   return distance;
 }
 
-void blink(int r, int g,int b, int interval, int num) {
-
+void blink(int r, int g, int b, int interval, int num) {
+  static bool state = false;
 
   if (currentMillis - previousMillis >= interval) {
-    
+
     previousMillis = currentMillis;
 
-    static bool state = false;
     state = !state;
+    ring.clear();
+    if (num != 0) {
+      if (state) {
+        ring.fill(ring.Color(r, g, b), 0, num);
+      }
 
-    if (state) {
-      ring.fill(ring.Color(r, g, b), 0, num);
-    } else {
-      ring.fill(ring.Color(r, g, b), 0, num);
+      ring.show();
     }
-    ring.show(); // Update Neopixel Ring
   }
 }
 
 
+float readTemperature() {
+  Wire.beginTransmission(SENSOR_ADDRESS);
+  Wire.write(0);
+  Wire.endTransmission();
 
+  delay(100);
 
+  Wire.requestFrom(SENSOR_ADDRESS, 2);
+  if (Wire.available() == 2) {
+    byte msb = Wire.read();
+    byte lsb = Wire.read();
 
+    int tempRaw = ((msb << 8) | lsb) >> 4;
+    float temperatureC = tempRaw * 0.0625;
+
+    return temperatureC;
+  } else {
+
+    Serial.println("Error: Data not available");
+    return -273.15;
+  }
+}
